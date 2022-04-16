afda7aaeb348421bd1431ff81a7cc0df93af4dee
Umar Farooq <62417503+Umar-Farooq-Shafi@users.noreply.github.com>
2022-04-16 12:56:10 +0500

    user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { UserModule } from './user/user.module';
+
 @Module({
-  imports: [],
+  imports: [UserModule],
   providers: [AppService],
   controllers: [AppController],
 })
